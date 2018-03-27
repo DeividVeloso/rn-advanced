dbd6c3b194f741953bcc2e689623c231a8b0d377
DeividVeloso <veloso.deivid@gmail.com>
2018-03-27 15:25:25 -0300

    add animation with interpolation and rotate

--- swipe/src/Deck.js
+++ swipe/src/Deck.js
@@ -4,29 +4,53 @@ import { View, Animated, PanResponder } from 'react-native';
 class Deck extends Component {
   constructor(props) {
     super(props);
+    //Não vamos setar o valor inicial de 0, vamos pegar do PanResponder
+    const position = new Animated.ValueXY();
     const panResponder = PanResponder.create({
       onStartShouldSetPanResponder: () => true,
       onPanResponderMove: (event, gesture) => {
-        debugger
-        console.log('Gesture', gesture);
+        //Passando a posição do Elemento x y e atualizando conforme se move.
+        position.setValue({ x: gesture.dx, y: gesture.dy });
       },
       onPanResponderRelease: () => {}
     });
 
-    this.state = { panResponder };
+    this.state = { panResponder, position };
   }
+
+  getCardStyle() {
+    const { position } = { ...this.state };
+
+    const rotate = position.x.interpolate({
+      inputRange: [-500, 0, 500],
+      outputRange: ['-120deg', '0deg', '120deg']
+    });
+
+    return {
+      ...position.getLayout(),
+      transform: [{ rotate }]
+    };
+  }
+
   renderCards() {
-    return this.props.data.map(item => {
+    return this.props.data.map((item, index) => {
+      if (index === 0) {
+        return (
+          <Animated.View
+            key={item.id}
+            style={this.getCardStyle()}
+            {...this.state.panResponder.panHandlers}
+          >
+            {this.props.renderCard(item)}
+          </Animated.View>
+        );
+      }
       return this.props.renderCard(item);
     });
   }
 
   render() {
-    // Esse panHandlers tem muitas funções de callbacks 
-    // que vai ajudar interceptar os pressionamentos do usuário na tela
-    return (
-      <View {...this.state.panResponder.panHandlers}>{this.renderCards()}</View>
-    );
+    return <View>{this.renderCards()}</View>;
   }
 }
 export default Deck;
